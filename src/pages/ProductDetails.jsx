6579540201b3b58cfb2e8c72f0b196889fae48f1
shemshajili <shamsah@code.edu.az>
2023-09-09 20:04:41 +0400

    Nav Part Added

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetails = () => {
+    return (
+        <div>
+            	product
+        </div>
+    );
+}
+
+export default ProductDetails;
---
+++ src/routers/Routers.js
@@ -0,0 +1,24 @@
+import {Routes,Route,Navigate} from 'react-router-dom'
+
+import Home from '../pages/Home'
+import Shop from '../pages/Shop'
+import Cart from '../pages/Cart'
+import CheckOut from '../pages/CheckOut'
+import Login from '../pages/Login'
+import ProductDetails from '../pages/ProductDetails'
+import SignUp from '../pages/SignUp'
+
+const Routers = () => {
+    return <Routes>
+        <Route path='/' element={<Navigate to='/home'/>}/>
+        <Route path='home' element={<Home/>}/>
+        <Route path='shop' element={<Shop/>}/>
+        <Route path='cart' element={<Cart/>}/>
+        <Route path='checkout' element={<CheckOut/>}/>
+        <Route path='login' element={<Login/>}/>
+        <Route path='signup' element={<SignUp/>}/>
+        <Route path='shop/:id' element={<ProductDetails/>}/>
+    </Routes>
+};
+
+export default Routers;
