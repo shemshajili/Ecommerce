cc7b0c8ff4b54277c9ac3287e6c7c29312fd6881
shemshajili <shamsah@code.edu.az>
2023-09-10 20:43:16 +0400

    Fixxed problem in product list

--- src/components/UI/ProductsList.jsx
+++ src/components/UI/ProductsList.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ProductCard from './ProductCard';
 
 const ProductsList = ({ data }) => {
-    return (
-      <>
-        {data.map((item) => (
-          <ProductCard key={item.id} item={item} />
-        ))}
-      </>
-    );
-  };
-  
+  return (
+    <>
+      {data.map((item) => (
+        <ProductCard key={item.id} item={item} />
+      ))}
+    </>
+  );
+};
 
 export default ProductsList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,105 +1,103 @@
-
+import React, { useEffect, useState } from 'react';
 import Helmet from '../components/Helmet/Helmet';
-import { Container, Row,Col } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import layer1 from '../assets/images/blueimg.webp';
 import '../styles/home.css';
-import { Link	 } from 'react-router-dom';
-import {motion} from 'framer-motion'
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import Services from '../services/Services';
 import ProductsList from '../components/UI/ProductsList';
-import products from '../assets/data/products'
-import { useEffect, useState } from 'react';
-
+import products from '../assets/data/products';
 
 const Home = () => {
+  const [data, setData] = useState([]);
 
-    const [data, setData]=useState(products)
-    useEffect(()=>{
-        const filterProducts=products.filter((item)=>item.category==='Bags');
-        setData(filterProducts);
-    },[]);
-    
-    return (
-        <div>
-            {/* Page title */}
-            <Helmet title={"Home"} />
+  useEffect(() => {
+    const filteredProducts = products.filter((item) => item.category === 'bag');
+    setData(filteredProducts);
+  }, []);
 
-            {/* Parallax effect */}
-            <Parallax pages={1.45}>
-                {/* Background layer */}
-                <ParallaxLayer
-                    offset={0}
-                    speed={0}
-                    factor={0.1}
-                    style={{
-                        backgroundColor: 'transparent',
-                        height: '20px',
-                    }}
-                />
+  return (
+    <div>
+      {/* Page title */}
+      <Helmet title={"Home"} />
 
-                {/* Parallax background image */}
-                <ParallaxLayer
-                    offset={0}
-                    speed={0.5}
-                    factor={1.5}
-                    style={{
-                        backgroundImage: `url(${layer1})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        height: '100vh',
-                    }}
-                />
+      {/* Parallax effect */}
+      <Parallax pages={1.45}>
+        {/* Background layer */}
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          factor={0.1}
+          style={{
+            backgroundColor: 'transparent',
+            height: '20px',
+          }}
+        />
 
-                {/* Parallax content layer */}
-                <ParallaxLayer
-                    offset={0}
-                    speed={0.8}
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        flexDirection: 'column',
-                        height: '100vh',
-                    }}
-                >
-                    {/* Hero section */}
-                    <section className="hero__section">
-                        <Container>
-                            <Row>
-                                <div className="hero__content">
-                                    {/* Hero title */}
-                                    <h2>Industrial design meets fashion.</h2>
-                                </div>
-                            </Row>
-                        </Container>
-                    </section>
-                      {/* Hero description */}
-                      <p className="hero__description">Atypical leather goods</p>
-                     {/* Shop Now button */}
-                    <motion.button whileTap={{ scale: 1 }} className='buy_btn'>
-                        {/* Shop Now text with a link */}
-                     <span className='buy_btn_text'>
-                    <Link to='/shop'>Shop Now</Link>
-                    </span>
-                    </motion.button>
-                </ParallaxLayer>
-            </Parallax>
+        {/* Parallax background image */}
+        <ParallaxLayer
+          offset={0}
+          speed={0.5}
+          factor={1.5}
+          style={{
+            backgroundImage: `url(${layer1})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh',
+          }}
+        />
 
-            <Services/>
-            <section className="trending__products">
-                <Container>
-                    <Row>
-                        <Col lg='12' className='text-center'>
-                            <h2 className='section__title'>Obsessive Attention. Intelligent Effort.</h2>
-                            <p className="custom-paragraph">Functional handbags made of luxurious materials to improve people's lives in small but mighty ways.</p>
-                        </Col>
-                        <ProductsList data={data}/>
-                    </Row>
-                </Container>
-            </section>
-        </div>
-    );
+        {/* Parallax content layer */}
+        <ParallaxLayer
+          offset={0}
+          speed={0.8}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            height: '100vh',
+          }}
+        >
+          {/* Hero section */}
+          <section className="hero__section">
+            <Container>
+              <Row>
+                <div className="hero__content">
+                  {/* Hero title */}
+                  <h2>Industrial design meets fashion.</h2>
+                </div>
+              </Row>
+            </Container>
+          </section>
+          {/* Hero description */}
+          <p className="hero__description">Atypical leather goods</p>
+          {/* Shop Now button */}
+          <motion.button whileTap={{ scale: 1 }} className='buy_btn'>
+            {/* Shop Now text with a link */}
+            <span className='buy_btn_text'>
+              <Link to='/shop'>Shop Now</Link>
+            </span>
+          </motion.button>
+        </ParallaxLayer>
+      </Parallax>
+
+      <Services />
+      <section className="trending__products">
+        <Container>
+          <Row>
+            <Col lg='12' className='text-center'>
+              <h2 className='section__title'>Obsessive Attention. Intelligent Effort.</h2>
+              <p className="custom-paragraph">Functional handbags made of luxurious materials to improve people's lives in small but mighty ways.</p>
+            </Col>
+            <ProductsList data={data} />
+          </Row>
+        </Container>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
