ee84d6d9c096cf808fd00015b6d6f8c20e2b42d5
shemshajili <shamsah@code.edu.az>
2023-09-13 23:40:27 +0400

    Select product added cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -32,31 +32,37 @@ const Cart = () => {
                 <Container>
                     <Row>
                         <Col lg='9'>
-                            { cartItems.length === 0 ?
-                                    <h2 className='fs-4 text-center'>No item added to the cart</h2> :
-                                 <table className='table bordered'>
-                                     <thead>
-                                            <tr>
-                                                <th>Image</th>
-                                                <th>Title</th>
-                                                <th>Price</th>
-                                                <th>Qty</th>
-                                                <th>Delete</th>
-                                            </tr>
-                                    </thead>
-
-                                    <tbody>
-                                            <tr>
-                                                <td><img src={tdImg} alt="" /></td>
-                                                <td>Mini Flex Bag - Gummy</td>
-                                                <td>$682.00</td>
-                                                <td>2px</td>
-                                                <td>
-                                                    <i className="ri-delete-bin-line"></i>
-                                                </td>
-                                            </tr>
-                                    </tbody>
-                                </table>
+                            { cartItems.length === 0 ?( <h2 className='fs-4 text-center'>No item added to the
+                                     cart</h2>):(
+                                          <table className='table bordered'>
+                                          <thead>
+                                                 <tr>
+                                                     <th>Image</th>
+                                                     <th>Title</th>
+                                                     <th>Price</th>
+                                                     <th>Qty</th>
+                                                     <th >Delete</th>
+                                                 </tr>
+                                         </thead>
+     
+                                         <tbody>
+                                                {
+                                                 cartItems.map((item,index)=>(
+                                                    <tr key={index}>
+                                                    <td><img src={item.imgUrl} alt="" /></td>
+                                                    <td>{item.productName}</td>
+                                                    <td>${item.price}</td>
+                                                    <td>{item.quantity}px</td>
+                                                    <td>
+                                                        <motion.i whileHover={{scale:1.2}} className="ri-delete-bin-line"></motion.i>
+                                                    </td>
+                                                </tr>
+                                                 ))
+                                                }
+                                         </tbody>
+                                     </table>
+                                    )
+                               
                             }
                         </Col>
                         <Col lg='3'>
