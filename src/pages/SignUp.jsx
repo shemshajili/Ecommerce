cfee3c857c65f2458b8e3982293de40754c58f45
shemshajili <shamsah@code.edu.az>
2023-09-16 01:54:33 +0400

    added signup db configuation with firebase

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,8 +3,14 @@ import Helmet from '../components/Helmet/Helmet'
 import { Container,Row,Col,Form,FormGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import '../styles/login.css'
-import {  createUserWithEmailAndPassword } from "firebase/auth";
+import {  createUserWithEmailAndPassword,updateProfile } from "firebase/auth";
+import {ref,uploadBytesResumable,getDownloadURL} from 'firebase/storage'
+import {setDoc,doc} from 'firebase/firestore'
 import {auth} from '../firebase.config'
+import {storage} from '../firebase.config'
+import {db} from '../firebase.config'
+import {toast} from 'react-toastify'
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
 
@@ -14,26 +20,61 @@ const SignUp = () => {
     const [file,setFile]=useState(null)
     const [loading,setLoading]=useState(false)
 
+    const navigate=useNavigate()
+
 
     const signup=async (e)=>{
         e.preventDefault()
         setLoading(true)
 
         try {
+        
         const userCredential=await createUserWithEmailAndPassword(auth,
         email,password)
 
         const user =userCredential.user
-        console.log(user);
+        
+        const storageRef=ref(storage, `images/${Date.now()+username}`)
+        const uploadTask=uploadBytesResumable(storageRef,file)
 
-        } catch (error) {}
+        uploadTask.on((error)=>{
+        toast.error(error.message)
+        },()=>{
+            getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL)=>{
+                await updateProfile(user,{
+
+                    //UPDATE USER PROFILE
+                    displayName:username,
+                    photoURL:downloadURL,
+                })
+
+                //store user data in firestore database
+                await setDoc(doc(db,'users',user.uid),{
+                    uid:user.uid,
+                    displayName:username,
+                    email,
+                    photoURL:downloadURL,
+                })
+            })
+        })
+        
+        setLoading(false)
+        toast.success('Account created')
+        navigate('/login')
+        } catch (error) {
+            setLoading(false)
+            toast.error('something went wrong')
+        }
     }
 
     return <Helmet title='SignUp'>
         <section>
             <Container>
                 <Row>
-                    <Col lg='6' className='m-auto text-center'>
+                    {
+                        loading? <Col lg='12' className='text-center'><h5 className='fw-bold'>Loading.....</h5></Col>
+                        : (
+                            <Col lg='6' className='m-auto text-center'>
                         <h3 className='fw-bold fs-4'>SignUp</h3>
 
                         <Form className='auth__form' onSubmit={signup}>
@@ -67,10 +108,11 @@ const SignUp = () => {
                             <p>Already have an account? <Link to='/login'>Login</Link></p>
                         </Form>
                     </Col>
+                        )
+                    }
                 </Row>
             </Container>
         </section>
-
     </Helmet>
 }
 
