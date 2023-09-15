55a17db7ab8b205170a25e7cd2cda31d9f40bb07
shemshajili <shamsah@code.edu.az>
2023-09-16 02:14:39 +0400

    Login connection db with firebase succesfully

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,40 +1,80 @@
-import React, {useState} from 'react';
-import Helmet from '../components/Helmet/Helmet'
-import { Container,Row,Col,Form,FormGroup } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import '../styles/login.css'
+import React, { useState } from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import '../styles/login.css';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase.config';
+import { toast } from 'react-toastify';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
+  const signIn = async (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-    return <Helmet title='Login'>
-        <section>
-            <Container>
-                <Row>
-                    <Col lg='6' className='m-auto text-center'>
-                        <h3 className='fw-bold fs-4'>Login</h3>
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
 
-                        <Form className='auth__form'>
-                            <FormGroup className='form__group'>
-                                <input type="email" placeholder='Enter your email'
-                                 value={email} onClick={e=>setEmail(e.target.value)} />
-                            </FormGroup>
-                            <FormGroup className='form__group'>
-                                <input type="password" placeholder='Enter your password'
-                                value={password} onClick={e=>setPassword(e.target.value)} />
-                            </FormGroup>
+      const user = userCredential.user;
+      console.log(user);
+      setLoading(false);
+      navigate('/checkout');
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.message);
+    }
+  };
 
-                            <button type='submit' className='buy__btn auth__btn'>Login</button>
-                            <p>Don't have an account? <Link to='/signup'>Create an account</Link></p>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
+  return (
+    <Helmet title='Login'>
+      <section>
+        <Container>
+          <Row>
+            {loading ? (
+              <Col lg='12' className='text-center'>
+                <h5 className='fw-bold'>Loading....</h5>
+              </Col>
+            ) : (
+              <Col lg='6' className='m-auto text-center'>
+                <h3 className='fw-bold fs-4'>Login</h3>
 
+                <Form className='auth__form' onSubmit={signIn}>
+                  <FormGroup className='form__group'>
+                    <input
+                      type="email"
+                      placeholder='Enter your email'
+                      value={email}
+                      onChange={e => setEmail(e.target.value)} 
+                    />
+                  </FormGroup>
+                  <FormGroup className='form__group'>
+                    <input
+                      type="password"
+                      placeholder='Enter your password'
+                      value={password}
+                      onChange={e => setPassword(e.target.value)}
+                    />
+                  </FormGroup>
+
+                  <button type='submit' className='buy__btn auth__btn'>
+                    Login
+                  </button>
+                  <p>
+                    Don't have an account? <Link to='/signup'>Create an account</Link>
+                  </p>
+                </Form>
+              </Col>
+            )}
+          </Row>
+        </Container>
+      </section>
     </Helmet>
-}
+  );
+};
 
 export default Login;
