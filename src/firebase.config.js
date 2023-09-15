38e22a8ac0c8e4f24ae79f56b217ea5d62df6825
shemshajili <shamsah@code.edu.az>
2023-09-16 00:29:28 +0400

    Connection FireBase

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeWxmIQaTwpbIK7HjCaanMoWtp1T6hhLg",
+  authDomain: "voguevie-99e04.firebaseapp.com",
+  projectId: "voguevie-99e04",
+  storageBucket: "voguevie-99e04.appspot.com",
+  messagingSenderId: "208397965243",
+  appId: "1:208397965243:web:59b002be53ac6472f6569e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth= getAuth(app)
+
+export default app
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,6 +3,8 @@ import Helmet from '../components/Helmet/Helmet'
 import { Container,Row,Col,Form,FormGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import '../styles/login.css'
+import {  createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../firebase.config'
 
 const SignUp = () => {
 
@@ -10,6 +12,22 @@ const SignUp = () => {
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
     const [file,setFile]=useState(null)
+    const [loading,setLoading]=useState(false)
+
+
+    const signup=async (e)=>{
+        e.preventDefault()
+        setLoading(true)
+
+        try {
+        const userCredential=await createUserWithEmailAndPassword(auth,
+        email,password)
+
+        const user =userCredential.user
+        console.log(user);
+
+        } catch (error) {}
+    }
 
     return <Helmet title='SignUp'>
         <section>
@@ -18,22 +36,31 @@ const SignUp = () => {
                     <Col lg='6' className='m-auto text-center'>
                         <h3 className='fw-bold fs-4'>SignUp</h3>
 
-                        <Form className='auth__form'>
+                        <Form className='auth__form' onSubmit={signup}>
                             <FormGroup className='form__group'>
-                                <input type="text" placeholder='Username'
-                                 value={email} onChange={e=>setEmail((e).target.value)} />
+                            <input
+                            type="text"
+                            placeholder='Username'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            />                          
                             </FormGroup>
                             <FormGroup className='form__group'>
-                                <input type="email" placeholder='Enter your email'
-                                 value={username} onChange={(e)=>setUsername(e.target.value)} />
+                            <input
+                            type="email" 
+                            placeholder='Enter your email'
+                            value={email} 
+                            onChange={(e)=>setEmail(e.target.value)} />
                             </FormGroup>
                             <FormGroup className='form__group'>
-                                <input type="password" placeholder='Enter your password'
-                                value={password} onChange={(e)=>setPassword(e.target.value)} />
+                            <input type="password" 
+                            placeholder='Enter your password'
+                            value={password} 
+                            onChange={(e)=>setPassword(e.target.value)} />
                             </FormGroup>
                             <FormGroup className='form__group'>
-                                <input type="file"
-                                onChange={(e)=>setFile(e.target.files[0])} />
+                            <input type="file"
+                            onChange={(e)=>setFile(e.target.files[0])} />
                             </FormGroup>
 
                             <button type='submit' className='buy__btn auth__btn'>Create an Account</button>
