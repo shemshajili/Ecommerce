f421d7c626999ec7ea640d082efb6fb18847635a
shemshajili <shamsah@code.edu.az>
2023-09-18 22:47:45 +0400

    Css code added Dashboard

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import {Container,Row,Col} from 'reactstrap'
+import '../styles/dashboard.css'
 
 const Dashboard = () => {
     return (
-        <div>
-            
-        </div>
+       <section>
+        <Container>
+            <Row>
+                <Col className='lg-3'>
+                    <div className="revenue__box">
+                        <h5>Total Sales</h5>
+                        <span>$7890</span>
+                    </div>
+                </Col>
+                <Col className='lg-3'>
+                    <div className="order__box">
+                        <h5>Orders</h5>
+                        <span>7890</span>
+                    </div>
+                </Col>
+                <Col className='lg-3'>
+                <div className="order__box">
+                        <h5>Total Products</h5>
+                        <span>7890</span>
+                    </div>
+                </Col>
+                <Col className='lg-3'>
+                    <div className="order__box">
+                        <h5>Total Users</h5>
+                        <span>7890</span>
+                    </div></Col>
+            </Row>
+        </Container>
+       </section>
     );
 }
 
